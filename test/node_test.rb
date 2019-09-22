7ae8c0b6b2e8ab75a747c004b5d1d9e242a541b7
Tylor Schafer <tylorschafer@gmail.com>
2019-09-22 12:34:45 -0600

    Add node class test file

---
+++ test/node_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/node'
+
+def setup
+  @node = Node.new("plop")
+end
+
+def test_it_exists
+  assert_instance_of Node, @node
+end
+
+def test_attributes
+  assert 'plop', @node.data
+end
+
+def test_next_node
+  assert_nil @node.next_node
+end
